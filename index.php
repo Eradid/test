2e23cc1a0f497e33663b55ab05c72e733074d35b
Eradid <corentin.montiege@gmail>
2020-01-30 15:28:04 +0100

    test

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="">
+	<![endif]-->
+	<!--[if IE 7]>
+	<html class="no-js lt-ie9 lt-ie8" lang="">
+		<![endif]-->
+		<!--[if IE 8]>
+		<html class="no-js lt-ie9" lang="">
+			<![endif]-->
+			<!--[if gt IE 8]>
+			 -->
+			<html class="no-js" lang="fr">
+				<!--
+				<![endif]-->
+				<head>
+					<meta charset="utf-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge">
+					<title>Reg'alade</title>
+					<meta name="description" content="">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<link rel="apple-touch-icon" href="apple-touch-icon.png">
+					<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+					<link rel="shortcut icon" type="image/png" href="http://reguiny.escalade.free.fr/images/logo.png"/>
+					<!--
+					<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+					-->
+					<!--For Plugins external css-->
+					<link rel="stylesheet" href="assets/css/plugins.css" />
+					<link rel="stylesheet" href="assets/css/raleway-webfont.css" />
+					<!--Theme custom css -->
+					<link rel="stylesheet" href="assets/css/style.css">
+					<!--Theme Responsive css-->
+					<link rel="stylesheet" href="assets/css/responsive.css" />
+					<?php include 'fonct.php' ?>
+					<script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+					<script type="text/javascript" src="fonction.js"></script>
+				</head>
+				<body>
+					<div class='preloader'>
+						<div class='loaded'>
+							&nbsp;
+						</div>
+					</div>
+					<nav class="navbar navbar-default navbar-fixed-top">
+						<div class="container menu">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse text-center" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav navbar-right">
+									<li class="active"><a href="index.php">Accueil</a></li>
+									<li><a href="le_club.php">Le Club</a></li>
+									<li><a href="membres.php">Nos compétiteurs</a></li>
+									<li><a href="inscription.php">Inscription</a></li>
+									<li><a href="contact.php">Contact</a></li>
+								</ul>
+							</div>
+							<!-- /.navbar-collapse -->
+						</div>
+						<!-- /.container-fluid -->
+					</nav>
+					<!--Home page style-->
+					<header id="home" class="home">
+						<div class="overlay-img">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-12">
+										<a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"/></a>
+										<div class="home-content titre">
+											<h5>
+												Le club d'escalade de Réguiny
+											</h5>
+										</div>
+										<div class="container text-center car">
+											<div class="img_label">
+												<img src="images/label-ffme.png" alt="label ffme" />
+												<img src="images/label_escalade_ecole.png" class="right" alt="label école" />
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</header>
+					<!-- Sections -->
+					<section class="sections">
+						<div class="text-center accueil">
+							<div class="text text-center">
+								 <h2>Actus</h2>
+							</div>
+							<iframe class="fb1" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fregalade.reguiny%2F&tabs=timeline&width=500&height=499&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="500" height="499" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+							<iframe class="fb2" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fregalade.reguiny%2F&tabs=timeline&width=340&height=499&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId" width="340" height="499" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+						</div>
+					</section>
+					<section class="sections">
+						<div class="about-bg">
+							<div class="container histoire">
+								<!-- Example row of columns -->
+								<div class="row">
+									<div class="col-md-6 col-sm-6 col-xs-6 histoire-text">
+										<div class="about-content" id="club">
+											<h3>L'histoire du club</h3>
+											<p>
+												Le club d'escalade de Réguiny est situé dans le centre Morbihan, à environ une vingtaine de kilomètres de Pontivy et de Locminé. Il a été créé en 1996, sous l'impulsion de Christophe Jousselin.
+												<br>
+												Ce club a pour but de promouvoir l'escalade aussi bien sur structures artificielles d'escalade (SAB) qu'en site naturel. Nous nous engageons à vous inculquer les différentes techniques de sécurité ainsi que toute la gestuelle technique nécessaire afin de vous permettre d'être autonome dans la pratique de ce sport. 
+												<br>
+												Nous disposons d'une SAE allant de 9 à 11m de hauteur, mais également d'un espace bloc de 95 m² et de 4m de hauteur.
+												<br>
+												Le club est affilié à la fédération française de la montagne et de l'escalade (FFME) et agrée par le ministère de la jeunesse et des sports.
+												<br>
+												En 2010 nous avons obtenu 2 labels FFME: label escalade et école d'escalade, gage de qualité et de dynamisme du club, en 2013, la FFME nous a renouvellé les 2 labels.
+												<br>
+												Le club organise aussi des sorties en falaise ou sur bloc (ex: Fontainebleau,Pen Hir, Inzinzac lochrist, l'île aux pies, Pyrénées, et..., le site le plus proche se situe dans les côtes d'Amor à la prénessaye, il est actuellement en cours d'équipement.
+												<br>
+												Les sorties en extérieur sont prévues à l'avance, vous trouverez donc les dates sur le calendrier du site ou bien sur le tableau d'affichage à la salle, toutefois ces dates sont en fonction des conditions météorologiques.
+											</p>
+										</div>
+									</div>
+									<div class="col-md-6 col-sm-6 col-xs-6 mur_vide">
+										<div class="about-img">
+											<img src="images/reguiny.jpg" alt="mur" />
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- /container -->
+						</div>
+					</section>
+					<!-- Sections -->
+					<!-- /container -->
+					<section id="about" class="sections">
+						<div class="heading-content text-center carte">
+							<h3>Localisation</h3>
+							<div class="separator"></div>
+							<p>
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2670.8133337164622!2d-2.7510289988830703!3d47.978666992649984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48102a10ad497313%3A0x610915bf340e6236!2sSalle+Des+Sports!5e0!3m2!1sfr!2sfr!4v1549725261756" style="border:0" allowfullscreen></iframe>
+							</p>
+						</div>
+					</section>
+					<footer id="footer" class="sections footer different-bg"></footer>
+					<footer id="our-client" class="copyright-footer sections different-bg">
+						<div class="container info">
+							<div class="row">
+								<div class="col-md-3 col-sm-6 col-xs-12">
+									<div class="footer-mid-content">
+										<h4>Contacts</h4>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Tel
+												</h6>
+												<p>
+													06 722 683 40
+												</p>
+											</div>
+										</div>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Mail
+												</h6>
+												<p>
+													reguiny.escalade@free.fr
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6 col-xs-12 site">
+									<div class="footer-mid-content">
+										<h4>Réseaux sociaux</h4>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													facebook
+												</h6>
+												<a href="https://www.facebook.com/regalade.reguiny/" target="_blank">Reg'alade</a>
+											</div>
+										</div>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Instagram
+												</h6>
+												<a href="https://www.instagram.com/reg_alade/?hl=fr" target="_blank">Reguiny Escalade</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6 col-xs-12">
+									<div class="footer-mid-content">
+										<h4>Sites amis</h4>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Le site de la fédé
+												</h6>
+												<a href="https://www.ffme.fr/" target="_blank">FFME</a>
+											</div>
+										</div>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Le club de Grand-Champ
+												</h6>
+												<a href="https://www.gregam-vertical.fr/" target="_blank">Grégam Vertical</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6 col-xs-12">
+									<div class="footer-mid-content">
+									<h4> &nbsp; </h4>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Le club de Vannes
+												</h6>
+												<a href="http://www.escalade5plus.fr/" target="_blank">Escalade 5+</a>
+											</div>
+										</div>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Le club de Séné
+												</h6>
+												<a href="https://senescalade.bzh/" target="_blank">Sénescalade</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6 col-xs-12">
+									<div class="footer-mid-content">
+									<h4> &nbsp;</h4>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Le club de Loudéac
+												</h6>
+												<a href="http://escalarmor.over-blog.com/" target="_blank">ESCAL'ARMOR</a>
+											</div>
+										</div>
+										<div class="post">
+											<div class="post-item">
+												<h6>
+													Le club de Mur de Bretagne
+												</h6>
+												<a href="https://guerledanescalade.clubeo.com/" target="_blank">Guerledan Escalade</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</footer>
+					<!--Footer-->
+					
+					<script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+					<script src="assets/js/vendor/bootstrap.min.js"></script>
+					<script src="assets/js/jquery.easypiechart.min.js"></script>
+					<script src="assets/js/plugins.js"></script>
+					<script src="assets/js/modernizr.js"></script>
+					<script src="assets/js/main.js"></script>
+				</body>
+			</html>
+
