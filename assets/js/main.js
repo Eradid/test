f8c822c2d442f2a6ba864d7ca407b20226e2cc92
Eradid <corentin.montiege@gmail>
2020-01-30 16:09:18 +0100

    4

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@
 
 jQuery(document).ready(function ($) {
 
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
     /*---------------------------------------------*
      * Mobile menu
      ---------------------------------------------*/
